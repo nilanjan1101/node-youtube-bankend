628ac1ef826245a0bf78fb464410312233a21b1d
Nilanjan <nilanjan.mech@gmail.com>
2024-10-26 00:07:49 +0530

    utils code setup

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 // require("dotenv").config({ path: "./env" });
 import dotenv from "dotenv";
 import connectDB from "./db/db.index.js";
+import { app } from "./app.js";
 
 dotenv.config({
   path: "./env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at PORT: ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("MongoDB Connection Failled!!!", error);
+  });
 
 /*
 import express from "express";
