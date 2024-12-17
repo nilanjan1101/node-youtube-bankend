2e33fd29c53357392f358b03f211e5f39bdca4a8
Nilanjan <nilanjan.mech@gmail.com>
2024-12-17 21:39:13 +0530

    Add comment, like, playlist, and tweet models

--- src/models/video.model.js
+++ src/models/video.model.js
@@ -1,5 +1,6 @@
-import mongoose, { Schema, Types } from "mongoose";
+import mongoose, { Schema } from "mongoose";
 import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
 const videoSchema = new Schema(
   {
     videoFile: {
